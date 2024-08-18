027254f46a7a31c26eb3b06275c40308f51c8df2
JaViKo500 <javikogutierrez64@gmail.com>
2024-08-18 11:26:58 -0500

    initialize firebase

--- lib/presentation/blocs/notifications/notifications_bloc.dart
+++ lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -1,6 +1,9 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+
+import 'package:push_app/firebase_options.dart';
 
 part 'notifications_event.dart';
 part 'notifications_state.dart';
@@ -15,6 +18,11 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
     // });
   }
 
+  static  Future<void> initializeFCM () async {
+    await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  }
   void requestPermissions() async {
     NotificationSettings settings = await messaging.requestPermission(
       alert: true,
