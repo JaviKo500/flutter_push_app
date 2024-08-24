c7ee995f46c258b8ee082459d5e5c084ed1679d5
JaViKo500 <javikogutierrez64@gmail.com>
2024-08-24 16:38:00 -0500

    background push notifications

--- lib/presentation/blocs/notifications/notifications_bloc.dart
+++ lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -8,6 +8,12 @@ import 'package:push_app/firebase_options.dart';
 part 'notifications_event.dart';
 part 'notifications_state.dart';
 
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+}
+
 class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
 
   FirebaseMessaging messaging = FirebaseMessaging.instance;
